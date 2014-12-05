a5d72057a9b71fee00f4c8175b5a8d100d54eb55
Norbert Podhorszki <pnorbert@ornl.gov>
2014-12-04 21:19:28 -0500

    Minor fix and add a python version of bpls

--- Tutorial/heat_transfer/python/heat_read.py
+++ Tutorial/heat_transfer/python/heat_read.py
@@ -2,30 +2,53 @@
 """
 Example:
 
-$ python ./heat_transfer.py
+$ python ./heat_transfer.py bpfile varname [varname [...]]
 """
 
 import adios as ad
 import numpy as np
+import sys
+
+def usage():
+    print "USAGE: %s bpfile varname" % sys.argv[0]
+
+fname = ""
+
+if len(sys.argv) < 3:
+    usage()
+    sys.exit(1)
+else:
+    fname = sys.argv[1]
+    varnames = sys.argv[2:]
 
 """ 
 Read all
 """
-T = ad.readvar("../heat.bp", "T")
-print ">>> shape:", T.shape
-print T
+print ">>> Read full data"
+for vname in varnames:
+    v = ad.readvar(fname, vname)
+    print ">>> name:", vname
+    print ">>> shape:", v.shape
+    print ">>> values:"
+    print v
 
 """ 
 Read step by step
 """
-f = ad.file("../heat.bp")
-v = f.var['T']
+print ""
+print ">>> Read step by step"
 
-for i in range(v.nsteps):
-    temp = v.read(from_steps=i)
+f = ad.file(fname)
+
+for i in range(f.current_step, f.last_step):
     print ">>> step:", i
-    print ">>> shape:", temp.shape
-    print temp
+    for vname in varnames:
+        v = f.var[vname]
+        val = v.read(from_steps=i)
+        print ">>> name:", vname
+        print ">>> shape:", v.dims
+        print ">>> values:"
+        print val
 
 f.close()
 
