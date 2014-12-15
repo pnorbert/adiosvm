747c84a5ae9bb8c7e87eef50aa9e2ae4ca6d8195
Jeremy Logan <jlogan7@utk.edu>
2014-12-15 15:29:13 -0500

    added parallel hdf5 writer (not working yet...

---
+++ Tutorial/heat_transfer/io_phdf5.F90
@@ -0,0 +1,107 @@
+
+!  ADIOS is freely available under the terms of the BSD license described
+!  in the COPYING file in the top level directory of this source distribution.
+!
+!  Copyright (c) 2008 - 2009.  UT-BATTELLE, LLC. All rights reserved.
+!
+!
+!  Write an ADIOS BP file 
+!
+! (c) Oak Ridge National Laboratory, 2009
+! Author: Norbert Podhorszki
+!
+module heat_io
+
+contains
+
+subroutine io_init()
+    use heat_vars
+    use HDF5
+
+    ! Initialize FORTRAN interface
+    call h5open_f(err)
+
+end subroutine io_init
+
+subroutine io_finalize()
+    use heat_vars
+    use HDF5
+
+    ! Close FORTRAN interface.
+    call h5close_f(err)
+
+end subroutine io_finalize
+
+subroutine io_write(tstep,curr)
+    use heat_vars
+    use HDF5
+    implicit none
+    include 'mpif.h'
+    integer, intent(in) :: tstep
+    integer, intent(in) :: curr
+
+    integer :: ndims
+    integer*8, dimension(1:2) :: dims
+
+    integer*8 io_size
+
+    INTEGER(HID_T) :: file_id
+    INTEGER(HID_T) :: dset_id
+    INTEGER(HID_T) :: dspace_id
+    INTEGER(HID_T) :: plist_id
+
+    INTEGER :: comm, info
+
+    character (len=200) :: filename
+    character(2) :: mode = "w"
+
+    comm = MPI_COMM_WORLD
+    info = MPI_INFO_NULL
+
+    write(filename,'(a,".h5")') trim(outputfile)
+    print '("rank ",i0," writes to: ",a)', rank, trim(filename)
+
+    call MPI_BARRIER(app_comm, err)
+    io_start_time = MPI_WTIME()
+
+    ndims = 2
+    dims(1) = ndx
+    dims(2) = ndy
+
+    ! Setup an access property list
+    call h5pcreate_f(H5P_FILE_ACCESS_F, plist_id, err)
+    call h5pset_fapl_mpio_f(plist_id, comm, info, err)
+
+    IF (tstep == 0) THEN
+
+        call h5fcreate_f (filename, H5F_ACC_TRUNC_F, file_id, err, access_prp = plist_id)
+        !need local and global dims
+        call h5screate_simple_f(ndims, dims, dspace_id, err)
+        call h5screate_simple_f(ndims, chunk_dims, memspace, error)
+        call h5dcreate_f(file_id, "T", H5T_NATIVE_DOUBLE, dspace_id, &
+                     dset_id, err)
+
+        io_size = 11*4 + 2*8*ndx*ndy 
+
+        call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, T(1:ndx,1:ndy,curr), dims, err)
+
+        ! close hdf5 objects
+        call h5pclose_f(plist_id, err)
+        call h5dclose_f(dset_id, err)
+        call h5sclose_f(dspace_id, err)
+        call h5fclose_f(file_id, err)
+
+    END IF
+
+    call MPI_BARRIER(app_comm ,err)
+    io_end_time = MPI_WTIME()
+    io_total_time = io_end_time - io_start_time
+    sz = io_size * nproc/1024.d0/1024.d0/1024.d0 !size in GB
+    gbs = sz/io_total_time
+    if (rank==0) print '("Step ",i3,": ",a20,d12.2,2x,d12.2,2x,d12.3)', &
+        tstep,filename,sz,io_total_time,gbs
+end subroutine io_write
+
+end module heat_io
+
+
