33032d36f58dc168e303514b68a68ac2426ebfba
Jeremy Logan <jlogan7@utk.edu>
2014-12-16 11:52:11 -0500

    parallel hdf5 write still buggy

--- Tutorial/heat_transfer/io_phdf5.F90
+++ Tutorial/heat_transfer/io_phdf5.F90
@@ -41,13 +41,22 @@ subroutine io_write(tstep,curr)
     integer, intent(in) :: curr
 
     integer :: ndims
-    integer*8, dimension(1:2) :: dims
+    !integer*8, dimension(1:2) :: dims !local chunk
+    integer*8, dimension(1:2) :: global_dims
+    integer*8, dimension(1:2) :: offsets 
+
+    INTEGER(HSIZE_T),  DIMENSION(2) :: count  
+    INTEGER(HSSIZE_T), DIMENSION(2) :: offset 
+    INTEGER(HSIZE_T),  DIMENSION(2) :: stride
+    INTEGER(HSIZE_T),  DIMENSION(2) :: dims 
+
 
     integer*8 io_size
 
     INTEGER(HID_T) :: file_id
     INTEGER(HID_T) :: dset_id
     INTEGER(HID_T) :: dspace_id
+    INTEGER(HID_T) :: memspace
     INTEGER(HID_T) :: plist_id
 
     INTEGER :: comm, info
@@ -68,27 +77,55 @@ subroutine io_write(tstep,curr)
     dims(1) = ndx
     dims(2) = ndy
 
-    ! Setup an access property list
-    call h5pcreate_f(H5P_FILE_ACCESS_F, plist_id, err)
-    call h5pset_fapl_mpio_f(plist_id, comm, info, err)
+    global_dims(1) = gndx 
+    global_dims(2) = gndy
+
+    offset(1) = offx
+    offset(2) = offy
+
+    stride(1) = 1 
+    stride(2) = 1 
+    count(1) =  1 
+    count(2) =  1
+
+    io_size = 11*4 + 2*8*ndx*ndy 
+
 
     IF (tstep == 0) THEN
 
+        call h5pcreate_f(H5P_FILE_ACCESS_F, plist_id, err)
+        call h5pset_fapl_mpio_f(plist_id, comm, info, err)
+
         call h5fcreate_f (filename, H5F_ACC_TRUNC_F, file_id, err, access_prp = plist_id)
-        !need local and global dims
-        call h5screate_simple_f(ndims, dims, dspace_id, err)
-        call h5screate_simple_f(ndims, chunk_dims, memspace, error)
+        call h5pclose_f(plist_id, err)
+
+        call h5screate_simple_f(ndims, global_dims, dspace_id, err)
+        call h5screate_simple_f(ndims, dims, memspace, err)
+
+        call h5pcreate_f(H5P_DATASET_CREATE_F, plist_id, err)
+        call h5pset_chunk_f(plist_id, ndims, dims, err)
         call h5dcreate_f(file_id, "T", H5T_NATIVE_DOUBLE, dspace_id, &
-                     dset_id, err)
+                     dset_id, err, plist_id)
+        call h5sclose_f(dspace_id, err)
 
-        io_size = 11*4 + 2*8*ndx*ndy 
+        call h5dget_space_f(dset_id, dspace_id, err)
+        call h5sselect_hyperslab_f (dspace_id, H5S_SELECT_SET_F, &
+                                    offsets, count, err, &
+                                    stride, dims)
 
-        call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, T(1:ndx,1:ndy,curr), dims, err)
+        call h5pcreate_f(H5P_DATASET_XFER_F, plist_id, err) 
+        call h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, err)
+
+        call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, T(1:ndx,1:ndy,curr), &
+                        global_dims, err, &
+                        file_space_id = dspace_id, mem_space_id = memspace, &
+                        xfer_prp = plist_id)
 
         ! close hdf5 objects
         call h5pclose_f(plist_id, err)
         call h5dclose_f(dset_id, err)
         call h5sclose_f(dspace_id, err)
+        call h5sclose_f(memspace, err)
         call h5fclose_f(file_id, err)
 
     END IF
