18ec655097be2da0ce7f3eef08a1b12ad29506a5
Norbert Podhorszki <pnorbert@ornl.gov>
2015-06-30 12:09:59 -0400

    Updates to heat transfer example for ADIOS 1.9

--- Tutorial/heat_transfer/io_adios_noxml.F90
+++ Tutorial/heat_transfer/io_adios_noxml.F90
@@ -24,6 +24,7 @@ subroutine io_init()
     call adios_allocate_buffer (20, ierr)
     call adios_declare_group (g, "heat", "", 1, ierr)
     call adios_select_method (g, "MPI", "", "", ierr)
+    !call adios_select_method (g, "MPI_AGGREGATE", "num_aggregators=2;num_ost=2;verbose=3", "", ierr)
 end subroutine io_init
 
 subroutine io_finalize()
