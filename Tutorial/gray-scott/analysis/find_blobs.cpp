3c8171a5d7594938d0b470c0981e337fddd1d0ba
Keichi Takahashi <keichi.t@me.com>
2019-02-23 15:19:01 +0900

    Add blob finder

---
+++ Tutorial/gray-scott/analysis/find_blobs.cpp
@@ -0,0 +1,156 @@
+#include <chrono>
+#include <iostream>
+
+#include <adios2.h>
+
+#include <vtkCellArray.h>
+#include <vtkConnectivityFilter.h>
+#include <vtkDataSetSurfaceFilter.h>
+#include <vtkMassProperties.h>
+#include <vtkPoints.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkThreshold.h>
+#include <vtkUnstructuredGrid.h>
+
+vtkSmartPointer<vtkPolyData> read_mesh(const std::vector<double> &bufPoints,
+                                       const std::vector<int> &bufCells)
+{
+    int nPoints = bufPoints.size() / 3;
+    int nCells = bufCells.size() / 3;
+
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    points->SetNumberOfPoints(nPoints);
+    for (vtkIdType i = 0; i < nPoints; i++) {
+        double x = bufPoints[i * 3 + 0];
+        double y = bufPoints[i * 3 + 1];
+        double z = bufPoints[i * 3 + 2];
+
+        points->SetPoint(i, x, y, z);
+    }
+
+    vtkSmartPointer<vtkCellArray> polys = vtkSmartPointer<vtkCellArray>::New();
+    for (vtkIdType i = 0; i < nCells; i++) {
+        vtkIdType a = bufCells[i * 3 + 0];
+        vtkIdType b = bufCells[i * 3 + 1];
+        vtkIdType c = bufCells[i * 3 + 2];
+
+        polys->InsertNextCell(3);
+        polys->InsertCellPoint(a);
+        polys->InsertCellPoint(b);
+        polys->InsertCellPoint(c);
+    }
+
+    vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
+    polyData->SetPoints(points);
+    polyData->SetPolys(polys);
+
+    return polyData;
+}
+
+void compute_curvature(const vtkSmartPointer<vtkPolyData> polyData)
+{
+    vtkSmartPointer<vtkConnectivityFilter> connectivityFilter =
+        vtkSmartPointer<vtkConnectivityFilter>::New();
+    connectivityFilter->SetInputData(polyData);
+    connectivityFilter->SetExtractionModeToAllRegions();
+    connectivityFilter->ColorRegionsOn();
+    connectivityFilter->Update();
+
+    int nBlobs = connectivityFilter->GetNumberOfExtractedRegions();
+    vtkSmartPointer<vtkThreshold> threshold =
+        vtkSmartPointer<vtkThreshold>::New();
+
+    vtkSmartPointer<vtkMassProperties> massProperties =
+        vtkSmartPointer<vtkMassProperties>::New();
+
+    std::cout << "Extracted "
+              << connectivityFilter->GetNumberOfExtractedRegions() << " blobs"
+              << std::endl;
+
+    vtkSmartPointer<vtkDataSetSurfaceFilter> surfaceFilter =
+        vtkSmartPointer<vtkDataSetSurfaceFilter>::New();
+
+    for (int i = 0; i < nBlobs; i++) {
+        threshold->SetInputData(connectivityFilter->GetOutput());
+        threshold->ThresholdBetween(i, i);
+        threshold->Update();
+
+        surfaceFilter->SetInputData(threshold->GetOutput());
+        surfaceFilter->Update();
+
+        massProperties->SetInputData(surfaceFilter->GetOutput());
+        massProperties->Update();
+
+        std::cout << "surface area of blob " << i << " is "
+                  << massProperties->GetSurfaceArea() << std::endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    MPI_Init(&argc, &argv);
+
+    if (argc < 4) {
+        std::cerr << "Too few arguments" << std::endl;
+        std::cout << "Usage: curvature input output step" << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, -1);
+    }
+
+    const std::string input_fname(argv[1]);
+    const std::string output_fname(argv[2]);
+    const int target_step = std::stoi(argv[3]);
+
+    adios2::ADIOS adios(MPI_COMM_WORLD);
+
+    adios2::IO inIO = adios.DeclareIO("IsosurfaceOutput");
+    adios2::Engine reader = inIO.Open(input_fname, adios2::Mode::Read);
+
+    adios2::IO outIO = adios.DeclareIO("CurvatureOutput");
+    adios2::Engine writer = outIO.Open(output_fname, adios2::Mode::Write);
+
+    std::vector<double> points;
+    std::vector<int> cells;
+    int step;
+
+    while (true) {
+        adios2::StepStatus status =
+            reader.BeginStep(adios2::StepMode::NextAvailable);
+
+        if (status != adios2::StepStatus::OK) {
+            break;
+        }
+
+        auto varPoint = inIO.InquireVariable<double>("point");
+        auto varCell = inIO.InquireVariable<int>("cell");
+        auto varStep = inIO.InquireVariable<int>("step");
+
+        varPoint.SetSelection(
+            {{0, 0}, {varPoint.Shape()[0], varPoint.Shape()[1]}});
+        varCell.SetSelection(
+            {{0, 0}, {varCell.Shape()[0], varCell.Shape()[1]}});
+
+        reader.Get<double>(varPoint, points);
+        reader.Get<int>(varCell, cells);
+        reader.Get<int>(varStep, &step);
+
+        reader.EndStep();
+
+        std::cout << "Step " << step << " " << varCell.Shape()[0] << " cells "
+                  << varPoint.Shape()[0] << " points" << std::endl;
+
+        vtkSmartPointer<vtkPolyData> polyData = read_mesh(points, cells);
+        auto start = std::chrono::steady_clock::now();
+        compute_curvature(polyData);
+        auto end = std::chrono::steady_clock::now();
+        auto diff = end - start;
+        auto duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(diff);
+
+        std::cout << "Found blobs in " << duration.count() << " [ms]"
+                  << std::endl;
+    }
+
+    writer.Close();
+    reader.Close();
+}
