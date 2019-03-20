27fb1d026ef638ce3ea4009a3271fb775e2bec15
Keichi Takahashi <keichi.t@me.com>
2019-03-20 16:18:14 +0900

    Make curvature compatible with latest isosurfacing

--- Tutorial/gray-scott/analysis/curvature.cpp
+++ Tutorial/gray-scott/analysis/curvature.cpp
@@ -1,32 +1,32 @@
 #include <chrono>
+#include <fstream>
 #include <iostream>
 
 #include <adios2.h>
 
 #include <vtkCellArray.h>
 #include <vtkCurvatures.h>
+#include <vtkDoubleArray.h>
+#include <vtkPointData.h>
 #include <vtkPoints.h>
 #include <vtkPolyData.h>
 #include <vtkSmartPointer.h>
-#include <vtkXMLPolyDataWriter.h>
+#include <vtkUnstructuredGrid.h>
 
 vtkSmartPointer<vtkPolyData> read_mesh(const std::vector<double> &bufPoints,
-                                       const std::vector<int> &bufCells)
+                                       const std::vector<int> &bufCells,
+                                       const std::vector<double> &bufNormals)
 {
     int nPoints = bufPoints.size() / 3;
     int nCells = bufCells.size() / 3;
 
-    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    auto points = vtkSmartPointer<vtkPoints>::New();
     points->SetNumberOfPoints(nPoints);
     for (vtkIdType i = 0; i < nPoints; i++) {
-        double x = bufPoints[i * 3 + 0];
-        double y = bufPoints[i * 3 + 1];
-        double z = bufPoints[i * 3 + 2];
-
-        points->SetPoint(i, x, y, z);
+        points->SetPoint(i, &bufPoints[i * 3]);
     }
 
-    vtkSmartPointer<vtkCellArray> polys = vtkSmartPointer<vtkCellArray>::New();
+    auto polys = vtkSmartPointer<vtkCellArray>::New();
     for (vtkIdType i = 0; i < nCells; i++) {
         vtkIdType a = bufCells[i * 3 + 0];
         vtkIdType b = bufCells[i * 3 + 1];
@@ -38,9 +38,16 @@ vtkSmartPointer<vtkPolyData> read_mesh(const std::vector<double> &bufPoints,
         polys->InsertCellPoint(c);
     }
 
-    vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
+    auto normals = vtkSmartPointer<vtkDoubleArray>::New();
+    normals->SetNumberOfComponents(3);
+    for (vtkIdType i = 0; i < nPoints; i++) {
+        normals->InsertNextTuple(&bufNormals[i * 3]);
+    }
+
+    auto polyData = vtkSmartPointer<vtkPolyData>::New();
     polyData->SetPoints(points);
     polyData->SetPolys(polys);
+    polyData->GetPointData()->SetNormals(normals);
 
     return polyData;
 }
@@ -55,29 +62,35 @@ void compute_curvature(const vtkSmartPointer<vtkPolyData> polyData)
     // curvaturesFilter->SetCurvatureTypeToGaussian();
     curvaturesFilter->SetCurvatureTypeToMean();
     curvaturesFilter->Update();
-
-    vtkSmartPointer<vtkXMLPolyDataWriter> writer =
-        vtkSmartPointer<vtkXMLPolyDataWriter>::New();
-    writer->SetInputData(curvaturesFilter->GetOutput());
-    writer->SetFileName("x.vtp");
-    writer->Write();
 }
 
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
 
-    if (argc < 4) {
-        std::cerr << "Too few arguments" << std::endl;
-        std::cout << "Usage: curvature input output step" << std::endl;
+    int rank, procs, wrank;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &wrank);
+
+    const unsigned int color = 7;
+    MPI_Comm comm;
+    MPI_Comm_split(MPI_COMM_WORLD, color, wrank, &comm);
+
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &procs);
+
+    if (argc < 3) {
+        if (rank == 0) {
+            std::cerr << "Too few arguments" << std::endl;
+            std::cout << "Usage: curvature input output" << std::endl;
+        }
         MPI_Abort(MPI_COMM_WORLD, -1);
     }
 
     const std::string input_fname(argv[1]);
     const std::string output_fname(argv[2]);
-    const int target_step = std::stoi(argv[3]);
 
-    adios2::ADIOS adios(MPI_COMM_WORLD);
+    adios2::ADIOS adios("adios2.xml", comm, adios2::DebugON);
 
     adios2::IO inIO = adios.DeclareIO("IsosurfaceOutput");
     adios2::Engine reader = inIO.Open(input_fname, adios2::Mode::Read);
@@ -87,7 +100,11 @@ int main(int argc, char *argv[])
 
     std::vector<double> points;
     std::vector<int> cells;
+    std::vector<double> normals;
     int step;
+
+    std::ofstream log("compute_curvature.log");
+    log << "step\tcompute_curvature" << std::endl;
 
     while (true) {
         adios2::StepStatus status =
@@ -99,6 +116,7 @@ int main(int argc, char *argv[])
 
         auto varPoint = inIO.InquireVariable<double>("point");
         auto varCell = inIO.InquireVariable<int>("cell");
+        auto varNormal = inIO.InquireVariable<double>("normal");
         auto varStep = inIO.InquireVariable<int>("step");
 
         varPoint.SetSelection(
@@ -108,26 +126,21 @@ int main(int argc, char *argv[])
 
         reader.Get<double>(varPoint, points);
         reader.Get<int>(varCell, cells);
+        reader.Get<double>(varNormal, normals);
         reader.Get<int>(varStep, &step);
 
         reader.EndStep();
 
-        std::cout << "Step " << step << " " << varCell.Shape()[0] << " cells "
-                  << varPoint.Shape()[0] << " points" << std::endl;
+        vtkSmartPointer<vtkPolyData> polyData =
+            read_mesh(points, cells, normals);
+        auto start = std::chrono::steady_clock::now();
+        compute_curvature(polyData);
+        auto end = std::chrono::steady_clock::now();
+        auto diff = end - start;
+        auto duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(diff);
 
-        if (step == target_step) {
-            vtkSmartPointer<vtkPolyData> polyData = read_mesh(points, cells);
-            auto start = std::chrono::steady_clock::now();
-            compute_curvature(polyData);
-            auto end = std::chrono::steady_clock::now();
-            auto diff = end - start;
-            auto duration =
-                std::chrono::duration_cast<std::chrono::milliseconds>(diff);
-
-            std::cout << "Computed curvature in " << duration.count() << " [ms]"
-                      << std::endl;
-            break;
-        }
+        log << step << "\t" << duration.count() << std::endl;
     }
 
     writer.Close();
--- Tutorial/gray-scott/run.sh
+++ Tutorial/gray-scott/run.sh
@@ -4,5 +4,6 @@ source ~/.bashrc
 
 ./build-macos/gray-scott simulation/settings.json &
 ./build-macos/isosurface gs.bp iso.bp 0.5 &
-./build-macos/find_blobs iso.bp &
+#./build-macos/find_blobs iso.bp &
+./build-macos/compute_curvature iso.bp curvature.bp &
 wait
